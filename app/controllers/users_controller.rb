336c65d5c2a8d966ff36da67225a22644c1873c3
viettien <viettienn97@gmail.com>
2017-10-16 20:53:25 +0700

    pass

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
 before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
 before_action :correct_user,   only: [:edit, :update]
 before_action :admin_user,     only: :destroy
-  
+
 
   def index
   @users = User.paginate(page: params[:page])
@@ -16,6 +16,7 @@ before_action :admin_user,     only: :destroy
 
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
 
   def new
@@ -58,7 +59,7 @@ before_action :admin_user,     only: :destroy
         store_location
         flash[:danger] = "Please log in."
         redirect_to login_url
-      end 
+      end
     end
 
      # Confirms the correct user.
